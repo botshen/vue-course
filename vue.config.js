e9940b4515841849b6b432558b792ebb7a4a0d32
Xin shen <363088847@qq.com>
2021-09-20 18:17:27 +0800

    创建基本目录

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/' : '/'
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  // 配置别名
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false,
+  // 解决跨域
+  devServer: {
+    proxy: 'http://localhost:3000'
+  }
 }
